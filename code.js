8d3c65813e8f9ce37d8d4653934b0b2f9cf44655
ZelayaGonzalo <zelayagonzalo33@gmail.com>
2020-11-28 14:34:07 -0300

    Can do basic operations.

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
+let firstNumber=0;
+let secondNumber=0;
+let operationID;
+let screenText=document.getElementById("screen");
+let curretnSign=document.getElementById("sign");
+screenText.value="0";
+erase();
+
 function add(a,b){
     return a+b;
 }
@@ -9,4 +17,114 @@ function multiply(a,b){
 }
 function divide(a,b){
     return a/b;
+}
+
+function addNumber(number){
+    if(number==-1){
+        if(screenText.value==0){
+            screenText.value=0+".";
+        }
+        else{
+        screenText.value+=".";
+        }
+    }
+    else{
+        if((screenText.value==0 && screenText.value!="0.")|| (operationID!=0)){
+            screenText.value=number;
+        }
+        else{
+            screenText.value+=number;
+        }
+    }
+    
+   
+}
+function backButton(){
+
+}
+
+
+function erase(){
+    firstNumber=0;
+    secondNumber=0;
+    screenText.value="0";
+    operationID=0;
+    showCurrentOperation(0);
+}
+
+function operate(sign){
+    if(operationID==0){
+        firstNumber=parseFloat(screenText.value);
+        operationID=sign;
+        //screenText.value="";
+    }
+    else{
+        equals();
+        operationID=sign;
+        firstNumber=parseFloat(screenText.value);
+
+    }
+    showCurrentOperation(sign);
+   
+}
+
+function equals(){
+    let result=0;
+    if(operationID!=0){
+        secondNumber=parseFloat(screenText.value);
+        if(operationID!=0){
+            switch(operationID){
+                case 1:
+                    result=add(firstNumber,secondNumber);
+                    break;
+                case 2:
+                    result=subtract(firstNumber,secondNumber);
+                    break;
+                case 3:
+                    result=multiply(firstNumber,secondNumber);
+                     break;
+                case 4:
+                    result=divide(firstNumber,secondNumber);
+                    break;
+                default:
+                    result="Error";
+                    break;
+            }
+        }
+        firstNumber=0;
+        secondNumber=0;
+        operationID=0;
+        screenText.value=result;
+
+    }
+    else{
+        result=screenText.value;
+    }
+    showCurrentOperation(0);
+   
+}
+
+function showCurrentOperation(id){
+    var currentOperation=id;
+    switch(id){
+        case 0:
+            curretnSign.innerHTML="";
+            break;
+        case 1:
+            curretnSign.innerHTML="+";
+            break;
+        case 2:
+            curretnSign.innerHTML="-";
+            break;
+        case 3:
+            curretnSign.innerHTML="*";
+            break;
+        case 4:
+            curretnSign.innerHTML="/";
+            break;
+        default:
+            curretnSign="";
+            break;
+            
+    }
 }
